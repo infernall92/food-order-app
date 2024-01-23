8befb9b04b5903316540767925413674f6b27bec
Hristo <infernall92@gmail.com>
2024-01-23 15:04:34 +0200

    Adding reusable custom Input component and managing Modal visibility

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,7 @@
 import Modal from "./UI/Modal.jsx";
 import CartContext from "../store/CartContext.jsx";
-import { currencyFormatter } from '../util/formatting.js'
-import Button from './UI/Button.jsx'
+import { currencyFormatter } from "../util/formatting.js";
+import Button from "./UI/Button.jsx";
 import { useContext } from "react";
 import UserProgressContext from "../store/UserProgressContext.jsx";
 import CartItem from "./CartItem.jsx";
@@ -10,31 +10,46 @@ export default function Cart() {
   const cartCtx = useContext(CartContext);
   const userProgressCtx = useContext(UserProgressContext);
 
-  const cartTotal = cartCtx.items.reduce((totalPrice, item) => totalPrice + item.quantity * item.price, 0)
+  const cartTotal = cartCtx.items.reduce(
+    (totalPrice, item) => totalPrice + item.quantity * item.price,
+    0
+  );
 
   function handleCloseCart() {
     userProgressCtx.hideCart();
   }
 
+  function handleGoToCheckout() {
+    userProgressCtx.showCheckout();
+  }
+
   return (
-    <Modal className="cart" open={userProgressCtx.progress === 'cart'}>
+    <Modal
+      className="cart"
+      open={userProgressCtx.progress === "cart"}
+      onClose={userProgressCtx.progress === "cart" ? handleCloseCart : null}
+    >
       <h2>Your cart</h2>
       <ul>
         {cartCtx.items.map((item) => (
-          <CartItem 
-          key={item.id} 
-          name={item.name} 
-          quantity={item.quantity} 
-          price={item.price} 
-          onIncrease={() => cartCtx.addItem(item)}
-          onDecrease={() => cartCtx.removeItem(item.id)} 
-           />
+          <CartItem
+            key={item.id}
+            name={item.name}
+            quantity={item.quantity}
+            price={item.price}
+            onIncrease={() => cartCtx.addItem(item)}
+            onDecrease={() => cartCtx.removeItem(item.id)}
+          />
         ))}
       </ul>
       <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
       <p className="modal-actions">
-        <Button textOnly onClick={handleCloseCart}>Close</Button>
-        <Button onClick={handleCloseCart}>Go to Checkout</Button>
+        <Button textOnly onClick={handleCloseCart}>
+          Close
+        </Button>
+        {cartCtx.items.length > 0 && (
+          <Button onClick={handleGoToCheckout}>Go to Checkout</Button>
+        )}
       </p>
     </Modal>
   );
